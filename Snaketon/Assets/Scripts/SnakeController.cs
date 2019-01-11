7252ecaa921d9ee424287ba8c74bc30a418537ae
Ismael Rodriguez <ismaelcentella@gmail.com>
2019-01-11 14:43:30 +0100

    Acabado Tutorial + Efectos al morir
    Se ha implementado:
    - Recoger items
    - Marcador de puntos
    - Efectos con rigidbodys a todos los bloques para explosionarlos
    + Funcionalidad mia: cuando mueres aparte del efecto ese hemos hecho un efecto que los colores parpadean, esto se irá mejorando y implementando en los lugares adecuados.
    ** Se quiere desarrollar un nivel 2 con enemigos que se mueven **

--- Snaketon/Assets/Scripts/SnakeController.cs
+++ Snaketon/Assets/Scripts/SnakeController.cs
@@ -1,26 +1,32 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SnakeController : MonoBehaviour {
 
-    public GameObject Bloque;
-    public GameObject Escenario;
-    public int Alto, Ancho;
+    public GameObject Bloque; // prefab del bloque
+    public GameObject Escenario; // prefab del escenario
+    public int Alto, Ancho; //Dimensiones de la pantalla
+    public GameObject Item; //prefab del item
+    public Text Puntuacion; // Texto del canvas de la puntuacion
+    public float FuerzaExplosion = 200f;
 
-    private Queue<GameObject> cuerpo = new Queue<GameObject>();
+    private Queue<GameObject> cuerpo = new Queue<GameObject>(); //cola de objetos que conforman la serpiente
     private GameObject cabeza;
+    private GameObject item; //item instanciado en la escena
 
-    private Vector3 direccion = Vector3.right;
+    private Vector3 direccion = Vector3.right; //direccion que vaya por defecto hacia la derecha
 
     private enum TipoCasilla
     {
-        Vacio, Obstaculo
+        Vacio, Obstaculo, Item
     }
 
     private TipoCasilla[,] mapa;
+
+    private int puntos = 0;
 
     private void Awake()
     {
@@ -31,7 +37,14 @@ public class SnakeController : MonoBehaviour {
         InicializaSerpiente(posicionInicialX, posicionInicialY);
         //siempre tendremos referencia a la cabeza:
         cabeza = NuevoBloque(posicionInicialX, posicionInicialY);
+        InstanciarItemEnPosicionAleatoria();
         StartCoroutine(Movimiento());
+    }
+
+    private void IncrementarPuntos()
+    {
+        puntos++;
+        Puntuacion.text = puntos.ToString();
     }
 
     private void Update()
@@ -44,6 +57,35 @@ public class SnakeController : MonoBehaviour {
         {
             direccion = direccionSeleccionada; //guardaremos la posicion siguiente para cuando nos movamos (Movimiento())
         }
+    }
+
+    private void MoverItemAPosicionAleatoria()
+    {
+        Vector3 posicion = ObtenerPosicionVaciaAleatoria();
+        item.transform.position = posicion;
+        EstablecerMapa(item.transform.position, TipoCasilla.Item);
+    }
+
+    private void InstanciarItemEnPosicionAleatoria()
+    {
+        Vector3 posicion = ObtenerPosicionVaciaAleatoria();
+        item = NuevoItem(Mathf.RoundToInt(posicion.x), Mathf.RoundToInt(posicion.y));
+    }
+
+    private Vector3 ObtenerPosicionVaciaAleatoria()
+    {
+        List<Vector3> posicionesVacias = new List<Vector3>();
+        for (int x = 0; x < Ancho; x++) //Doble bucle para rellenar con bloques
+        {
+            for (int y = 0; y < Alto; y++)
+            {
+                if(mapa[x, y] == TipoCasilla.Vacio)
+                {
+                    posicionesVacias.Add(new Vector3(x, y));
+                }
+            }
+        }
+        return posicionesVacias[Random.Range(0, posicionesVacias.Count)];
     }
 
     private TipoCasilla ObtenerMapa(Vector3 posicion)
@@ -63,6 +105,13 @@ public class SnakeController : MonoBehaviour {
         cuerpo.Enqueue(nuevo);
         //Lo ponemos como ocupado ese bloque que hayamos construido
         EstablecerMapa(nuevo.transform.position, TipoCasilla.Obstaculo);
+        return nuevo;
+    }
+
+    private GameObject NuevoItem(int x, int y)
+    {
+        GameObject nuevo = Instantiate(Item, new Vector3(x, y), Quaternion.identity, Escenario.transform);
+        EstablecerMapa(nuevo.transform.position, TipoCasilla.Item);
         return nuevo;
     }
 
@@ -90,18 +139,28 @@ public class SnakeController : MonoBehaviour {
 
             if(casillaAOcupar == TipoCasilla.Obstaculo) //si la casilla siguiente es un obstaculo
             {
-                Debug.Log("Muerto!");
+                Muerte();
                 yield return new WaitForSeconds(5); //espera 5 segundos
                 SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); //recarga la escena
                 yield break; //sal de la corrutina
             }
             else
             {
-                GameObject parteCuerpo = cuerpo.Dequeue(); //sacara la primera de la cola
-                EstablecerMapa(parteCuerpo.transform.position, TipoCasilla.Vacio); //antes de moverlo está vacío
-                parteCuerpo.transform.position = nuevaPosicion; // a su posicion le pondrá la siguiente posicion que le corresponde
-                EstablecerMapa(parteCuerpo.transform.position, TipoCasilla.Obstaculo); //después de moverlo ya lo hemos rellenado
-                cuerpo.Enqueue(parteCuerpo); //lo encolamos el siguiente cacho
+                GameObject parteCuerpo = new GameObject();
+                if(casillaAOcupar == TipoCasilla.Item)
+                {
+                    parteCuerpo = NuevoBloque(nuevaPosicion.x, nuevaPosicion.y);
+                    MoverItemAPosicionAleatoria();
+                    IncrementarPuntos();
+                }
+                else
+                {
+                    parteCuerpo = cuerpo.Dequeue(); //sacara la primera de la cola
+                    EstablecerMapa(parteCuerpo.transform.position, TipoCasilla.Vacio); //antes de moverlo está vacío
+                    parteCuerpo.transform.position = nuevaPosicion; // a su posicion le pondrá la siguiente posicion que le corresponde
+                    EstablecerMapa(parteCuerpo.transform.position, TipoCasilla.Obstaculo); //después de moverlo ya lo hemos rellenado
+                    cuerpo.Enqueue(parteCuerpo); //lo encolamos el siguiente cacho
+                }
 
                 cabeza = parteCuerpo; //ahora la cabeza será esa pieza
 
@@ -127,4 +186,48 @@ public class SnakeController : MonoBehaviour {
         }
     }
 
+    private void Muerte()
+    {
+        Explotar(this.GetComponentsInChildren<Rigidbody>());
+        Explotar(Escenario.GetComponentsInChildren<Rigidbody>());
+        StartCoroutine(CambiaColoresCamara());
+    }
+
+    private IEnumerator CambiaColoresCamara()
+    {
+        var vecesAEjecutarColores = 8;
+        var colores = new Color[]
+        {
+            Color.black,
+            Color.magenta,
+            Color.red,
+            Color.blue
+        };
+
+        for (int i = 0; i < vecesAEjecutarColores; i++)
+        {
+            var pos = Random.Range(0, colores.Length);
+            Color color = colores[pos];
+            Camera.main.backgroundColor = color;
+
+            if(i == vecesAEjecutarColores - 1)
+            {
+                yield break;
+            }
+            else
+            {
+                yield return new WaitForSeconds(0.3f);
+            }
+        }
+    }
+
+    private void Explotar(Rigidbody[] rbs)
+    {
+        foreach (Rigidbody r in rbs)
+        {
+            r.useGravity = true;
+            r.AddForce(Random.insideUnitCircle.normalized * FuerzaExplosion);
+            r.AddTorque(0, 0, Random.Range(-FuerzaExplosion, FuerzaExplosion));
+        }
+    }
 }
